920c5dfabae94a76745f4ed92f26d16ecba2501c
harvey magana <heyharv2012@gmail.com>
2021-01-27 18:18:10 -0800

    Added the first get request method to the accounts-router file.

--- api/accounts-router.js
+++ api/accounts-router.js
@@ -4,4 +4,18 @@ const Accounts = require('./accounts-model.js');
 
 const router = express.Router();
 
+router.get('/', async (req, res, next) => { 
+    try {
+        const data = await Accounts.get()
+        res.status(200).json(data);
+    } catch (err) {
+        next(err);
+    }
+});
+
+// router.get('/:id', (req, res) => { });
+// router.post('/', (req, res) => { });
+// router.put('/:id', (req, res) => { });
+// router.delete('/:id', (req, res) => { });
+
 exports.module = router;
